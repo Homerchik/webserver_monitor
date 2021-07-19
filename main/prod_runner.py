d0f6dec3b6ceb85e3bfbc77659ae27add9a05941
homerchik <homerchik@gmail.com>
2021-07-19 13:18:34 +0300

    make producer runner pretty

--- main/prod_runner.py
+++ main/prod_runner.py
@@ -1,22 +1,32 @@
 import logging
 
+import click
+
 from tools.log import init_logging
-from tools.utils import flatten, read_config, create_job
+from tools.utils import flatten, read_config, create_job, build_conf_logging_path, build_conf_app_path
 from workers.request_metrics import RequestMetrics
 from workers.workers import Publish
 from wrappers.producer import Producer
 
 
-if __name__ == "__main__":
-    init_logging()
-    config = read_config()
+@click.command()
+@click.option('--config-dir', prompt='Directory where application configs stored')
+def run(config_dir):
+    """Script that runs kafka producer. It checks webservices from config and push metric messages
+       about services state to Kafka"""
+    init_logging(build_conf_logging_path(config_dir))
+    config = read_config(build_conf_app_path(config_dir))
     sites = config.get('monitoring').items()
 
     payload = flatten([create_job(hostname, settings) for hostname, settings in sites])
-    producer = Producer()
-    work = [Publish(RequestMetrics(host, page, regexp), producer) for host, page, regexp in payload]
+    producer = Producer(configs=config)
+    work = [Publish(RequestMetrics(host, page, regexp), producer, config=config) for host, page, regexp in payload]
 
     logging.info(f"Metrics generator script init finished. Number of threads {len(work)}. Starting cycle...")
     for job in work:
         job.start()
+
+
+if __name__ == "__main__":
+    run()
 
